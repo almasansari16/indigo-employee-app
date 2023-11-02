ac174b62b556f1a9bbbff6b3352d81ca80fa161f
AlmasHanif <almashanif126@gmail.com>
2023-11-02 16:57:03 +0500

    cahnges

--- src/screens/barcodeScan/NewQrCode.js
+++ src/screens/barcodeScan/NewQrCode.js
@@ -27,6 +27,7 @@ export default function NewQrCode({ navigation }) {
         })();
     }, []);
 
+    
     useEffect(() => {
         if (isScanning && barcodes.length > 0) {
             scanCodes();
